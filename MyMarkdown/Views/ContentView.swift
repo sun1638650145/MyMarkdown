c357cf17b8ff5f9036bbefd2b9f132422884196d
sun1638650145 <1638650145@qq.com>
2021-11-12 14:55:58 +0800

    Fix typos and add README.md

--- MyMarkdown/Views/ContentView.swift
+++ MyMarkdown/Views/ContentView.swift
@@ -45,13 +45,15 @@ struct ContentView: View {
                     let markupText = UIMarkupTextPrintFormatter(markupText: html)
                     // 绘制要打印内容页面的渲染器.
                     let render = UIPrintPageRenderer()
-                    /* A4: 210 x 297mm (8-1/4 x 11-3/4 in) 在72dpi时, 像素值为595.3, 841.8 */
-                    let page = CGRect(x: 0.0, y: 0.0, width: 595.3, height: 841.9)
+                    /* A4: 210 x 297mm (8-1/4 x 11-3/4 in) 在72dpi时, 像素值为595.3, 841.9 */
+                    let pageRect = CGRect(x: 0.0, y: 0.0, width: 595.3, height: 841.9)
+                    /* Pages文稿 文稿页边空白 顶部32.5mm 底部35mm 左边20mm 右边20mm */
+                    let printableRect = CGRect(x: 56.7, y: 92.1, width: 481.9, height: 650.6)
                     
-                    // 从第0页开始向渲染器田间内容, 纸张大小A4, 打印区域大小A4.
+                    // 从第0页开始向渲染器添加内容, 纸张大小, 打印区域大小.
                     render.addPrintFormatter(markupText, startingAtPageAt: 0)
-                    render.setValue(page, forKey: "paperRect")
-                    render.setValue(page, forKey: "printableRect")
+                    render.setValue(pageRect, forKey: "paperRect")
+                    render.setValue(printableRect, forKey: "printableRect")
                     
                     // 链接到动态字节缓冲区.
                     let pdfData = NSMutableData()
@@ -72,7 +74,7 @@ struct ContentView: View {
                 }) {
                     HStack {
                         Text("导出PDF")
-                        Image(systemName: "rectangle.portrait.and.arrow.right")
+                        Image(systemName: "arrow.down.circle.fill")
                     }
                     .foregroundColor(.blue)
                     .font(.system(size: 12))
---
+++ README.md
@@ -0,0 +1,4 @@
+# MyMarkdown: iOS上的所见即所得的Markdown编辑器.
+
+
+
